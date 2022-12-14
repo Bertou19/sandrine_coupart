53fad102a03644ba0fbc068f0263e6f436de28b8
Bertille De Almeida <a_bertille@msn.com>
2022-12-14 18:21:50 +0100

    creation pages

---
+++ contact.php
@@ -0,0 +1,61 @@
+<?php
+session_start();
+
+$nav_en_cours = 'Contact';
+
+//On definit le titre
+$titrePrincipal = "Ajouter un patient";
+
+
+// On inclut le header
+@include_once "includes/header.php";
+// On inclut la navbar
+@include_once "includes/navbar.php";
+
+?>
+
+<div class="container">
+  <div class="row">
+    <div class="col-12 text-md-center">
+      <h1 class="title-form pt-5 text-md-center text-xs-center">Formulaire de contact</h1>
+
+    </div>
+  </div>
+</div>
+
+<section>
+  <form class="border border-warning rounded-5 " id="my-form" action="https://formspree.io/f/mbjbkrbw" method="POST">
+    <div class="form mb-1 p-5 text-center">
+      <label for="exampleFormControlInput1" class="form-label text-warning">E-mail</label>
+      <input name="email" type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+    </div>
+    <div class="form mb-2 p-2 text-center">
+      <label for="exampleFormControlTextarea1" class="form-label text-warning">Message</label>
+      <textarea name="message" class="form-control" id="exampleFormControlTextarea1" rows="3" type="text"></textarea>
+      <button class="btn btn-warning button text-light mt-2" id="my-form-button">Envoyer</button>
+      <p id="my-form-status"></p>
+    </div>
+  </form>
+</section>
+
+<section>
+  <div class="container-fluid ">
+    <div class="row justify-content-center">
+      <div class="col-12">
+        <h2 class="adresse-title text-warning text-center mt-1">Adresse du cabinet</h2>
+        <div class="text-center mb-5">
+          <p>56, Av des carottes </p>
+          <p> 12890 CAEN</p>
+          <p> 0098564346</p>
+          <p> sandrinecoupart@gmail.com</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php
+
+// On inclut le footer
+@include_once "includes/footer.php";
+?>
---
+++ mentions_legales.php
@@ -0,0 +1,48 @@
+<?php
+session_start();
+
+$nav_en_cours = 'Mentions_legales';
+
+
+
+//ON définit le titre
+$titre = "Mentions légales";
+
+//On inclut le header
+include "includes/header.php";
+
+//On inclut la navbar
+include "includes/navbar.php";
+
+//On écrit le contenu de la page
+
+?>
+<h1 class="mentions-title text-center m-5">Mentions légales</h1>
+
+<div class="container-fluid">
+  <div class="row justify-content-center">
+
+
+    <section class="col-6 text-center p-2 mb-3">
+      <p> Lorem ipsum dolor sit amet. Et quia nemo id sequi animi aut obcaecati iure et doloremque doloribus sit ipsa accusamus. Ab dolorem dolore et repellendus dolor et labore minima.
+        Cum architecto error ab autem voluptatem qui consequatur accusantium ut quia autem qui officia commodi qui deserunt voluptatibus.
+        Est galisum numquam sit nisi incidunt est animi eius non galisum internos in quia vero eum doloribus quas et modi illum.
+      </p>
+      <p>
+        Sit alias veniam qui praesentium voluptatem eum quos porro est nostrum sint qui ullam voluptate non iusto reiciendis? Est animi tenetur qui enim galisum non consequatur illo aut ipsa quia.
+        Eos praesentium unde et numquam consectetur est exercitationem doloribus in dolore repellat et voluptatibus ducimus rem enim enim est aperiam incidunt.
+      </p>
+      <p>
+        Et repellendus sapiente ut laboriosam expedita vel perspiciatis quos qui nulla voluptate non voluptas quia non excepturi facilis.
+        Ea exercitationem unde ut commodi obcaecati id doloribus nulla eum voluptas quis nam perspiciatis velit?
+        Ut deleniti mollitia et dolores consequatur vel sunt eaque At unde nesciunt 33 rerum quis ut Quis sint sed incidunt dolorem!
+        Et nisi optio ut quia eius et mollitia recusandae sit consequuntur nisi id voluptatem deserunt in atque omnis.
+      </p>
+    </section>
+  </div>
+</div>
+
+<?php
+//On inclut le footer
+include "includes/footer.php";
+?>
